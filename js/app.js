620cd8529b916946095e541a328c8002866fdaac
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2021-02-15 10:20:53 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+
+document.querySelector('.items-form').onsubmit = avoidSubmit
+
+function avoidSubmit(event) {
+    event.preventDefault()
+    getValue()
+}
+
+function getValue() {
+    let taskText = document.querySelector('.items-form input').value
+    insertTask(taskText)
+
+    document.querySelector('.items-form input').value = ''
+}
+
+function insertTask(taskText) {
+    tasksArray.push(taskText)
+    printTasks()
+}
+
+function printTasks() {
+
+    let taskCard = ''
+
+    tasksArray.forEach((eachTask, index) => {
+
+        taskCard += `<div class="item">
+                        <span>${eachTask}</span>
+                        <button class="delete-item" data-index="${index}">Realizada</button>
+                    </div>`
+    })
+
+    document.querySelector('.list-panel').innerHTML = taskCard
+
+    setEventListeners()
+}
+
+
+
+function setEventListeners() {
+    document.querySelectorAll('.delete-item').forEach(eachButton => eachButton.onclick = removeTask)
+}
+
+
+function removeTask() {
+    let taskID = this.dataset.index
+    tasksArray.splice(taskID, 1)
+    printTasks()
+}
